efb60e414bc7235af2f62c68f113a8ba5da964ec
tswt0825 <tswt0825@gmail.com>
2025-08-27 10:24:38 +0200

    ok

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+let homeScore = 0;
+let guestScore = 0;
 
+const homeDisplay = document.getElementById("homeScore");
+const guestDisplay = document.getElementById("guestScore");
+
+function updateScores() {
+  homeDisplay.textContent = homeScore;
+  guestDisplay.textContent = guestScore;
+}
+
+document.querySelectorAll(".home-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const points = parseInt(btn.dataset.points);
+    homeScore += points;
+    updateScores();
+  });
+});
+
+// Legg til eventListeners på GUEST-knappene
+document.querySelectorAll(".guest-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    guestScore += parseInt(btn.dataset.points);
+    updateScores();
+  });
+});
+
+updateScores();
+
+document.getElementById("resetBtn").addEventListener("click", () => {
+  homeScore = 0;
+  guestScore = 0;
+  updateScores();
+});
